d154796ded43687118bad1ae29751ff5f5463dbb
zhaoyue <383484192@qq.com>
2020-03-10 23:49:31 +0800

    tree node解答

---
+++ src/a543_tree/Main.java
@@ -0,0 +1,9 @@
+package a543_tree;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.diameterOfBinaryTree(null));
+    }
+}
